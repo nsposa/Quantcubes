c61b0b9529cf77cf82aec56e044420972d90b8d4
Kasey White <kwhitemsg@gmail.com>
2020-02-16 04:15:05 -0500

    [baseSetup]: stage 1 start Stripping out the unnecessary code to build a lean prototype

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import GUI from './containers/gui.jsx';
+import AppStateHOC from './lib/app-state-hoc.jsx';
+import GuiReducer, {guiInitialState, guiMiddleware, initEmbedded, initFullScreen, initPlayer} from './reducers/gui';
+import LocalesReducer, {localesInitialState, initLocale} from './reducers/locales';
+import {ScratchPaintReducer} from 'scratch-paint';
+import {setFullScreen, setPlayer} from './reducers/mode';
+import {remixProject} from './reducers/project-state';
+import {setAppElement} from 'react-modal';
+
+const guiReducers = {
+    locales: LocalesReducer,
+    scratchGui: GuiReducer,
+    scratchPaint: ScratchPaintReducer
+};
+
+export {
+    // Go to the gui.jsx file underneath src/containers
+    GUI as default,
+    AppStateHOC,
+    setAppElement,
+    guiReducers,
+    guiInitialState,
+    guiMiddleware,
+    initEmbedded,
+    initPlayer,
+    initFullScreen,
+    initLocale,
+    localesInitialState,
+    remixProject,
+    setFullScreen,
+    setPlayer
+};
